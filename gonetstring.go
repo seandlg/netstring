ef738b6dbd9febb4093229105b755b8032616691
Julian Morrison <julian.morrison@gmail.com>
2014-05-24 17:18:15 +0100

    Netstring struct

--- gonetstring.go
+++ gonetstring.go
@@ -1,1 +1,37 @@
+/*
+D. J. Bernstein's netstrings for Go.
+*/
 package gonetstring
+
+import (
+	"bytes"
+	"errors"
+)
+
+var Incomplete = errors.New("The netstring is incomplete")
+
+type Netstring struct {
+	length int64
+	buffer *bytes.Buffer
+}
+
+func From(buf []byte) *Netstring {
+	return &Netstring{
+		length: int64(len(buf)),
+		buffer: bytes.NewBuffer(buf),
+	}
+}
+
+func (n *Netstring) IsComplete() bool {
+	if n.length < 0 || n.buffer == nil {
+		return false
+	}
+	return n.length == int64(n.buffer.Len())
+}
+
+// Returns the advertized length of the netstring. If n.IsComplete() then this is the length of the data in the buffr, too.
+func (n *Netstring) Length() int64 { return n.length }
+
+func (n *Netstring) Bytes() []byte {
+	return n.buffer.Bytes()
+}
