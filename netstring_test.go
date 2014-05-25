0b1176fcaa9f62139d48acec5aa4badbcbd1450c
Julian Morrison <julian.morrison@gmail.com>
2014-05-25 02:27:45 +0100

    finish Testincomplete

--- netstring_test.go
+++ netstring_test.go
@@ -32,4 +32,33 @@ func TestReading(t *testing.T) {
 	}
 }
 
-func TestIncomplete(t *testing.T) {} // TODO
+func TestIncomplete(t *testing.T) {
+	n := ForReading()
+	input := bytes.NewBufferString("1")
+	err := n.ReadFrom(input)
+	if err != Incomplete {
+		t.Fatal(err)
+	}
+	input.WriteString("1:hello")
+	err = n.ReadFrom(input)
+	if err != Incomplete {
+		t.Fatal(err)
+	}
+	input.WriteString(" world")
+	err = n.ReadFrom(input)
+	if err != Incomplete {
+		t.Fatal(err)
+	}
+	input.WriteString(",")
+	err = n.ReadFrom(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, err := n.Bytes()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if "hello world" != string(out) {
+		t.Fatal(out)
+	}
+}
